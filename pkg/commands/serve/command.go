53e21ab40ca9f0e9dd3b4789f67c5616e95edbcc
aerosouund <aerosound161@gmail.com>
2025-09-13 01:29:40 +0300

    add control plane command
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- pkg/commands/serve/command.go
+++ pkg/commands/serve/command.go
@@ -2,6 +2,7 @@ package serve
 
 import (
 	authzserver "github.com/kyverno/kyverno-http-authorizer/pkg/commands/serve/authz-server"
+	controlplane "github.com/kyverno/kyverno-http-authorizer/pkg/commands/serve/control-plane"
 	sidecarinjector "github.com/kyverno/kyverno-http-authorizer/pkg/commands/serve/sidecar-injector"
 	validationwebhook "github.com/kyverno/kyverno-http-authorizer/pkg/commands/serve/validation-webhook"
 	"github.com/spf13/cobra"
@@ -10,10 +11,11 @@ import (
 func Command() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "serve",
-		Short: "Run Kyverno Envoy Plugin servers",
+		Short: "Run Kyverno HTTP Authorizer servers",
 	}
 	command.AddCommand(authzserver.Command())
 	command.AddCommand(sidecarinjector.Command())
 	command.AddCommand(validationwebhook.Command())
+	command.AddCommand(controlplane.Command())
 	return command
 }
